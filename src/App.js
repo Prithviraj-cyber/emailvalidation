ba0b7ee224ba204e2a51d09f5316d326f250eb0f
@anandwilsonwings <anand@wilsonwings.com>
2025-02-27 08:00:43 +0530

    second comment

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Dashboard from './pages/Dashboard';
 import Processing from './pages/Processing';
